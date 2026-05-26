00be32846d6d52e476480328dfff82971023e6b4
SodolynskyiVadym <vadimsodolinskij@gmail.com>
2026-05-26 16:06:24 +0300

    implemented delete MAUI

--- ClientApp/Services/ApiService.cs
+++ ClientApp/Services/ApiService.cs
@@ -1,4 +1,5 @@
 ﻿using ClientApp.Models;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
@@ -107,6 +108,7 @@ namespace ClientApp.Services
             }
         }
 
+
         public async Task<ApiResponse<AudioTask>> UpdateAnalysisAudioTaskAsync(string id, AudioTaskUpdate request)
         {
             try
@@ -115,6 +117,61 @@ namespace ClientApp.Services
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
                 var response = await _httpClient.PatchAsync($"{_url}/analysis/{id}", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var options = new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    options.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+
+                    var task = await response.Content.ReadFromJsonAsync<AudioTask>(options);
+                    return new ApiResponse<AudioTask> { Data = task, IsSuccess = true };
+                }
+
+                return new ApiResponse<AudioTask> { IsSuccess = false, ErrorMessage = response.ReasonPhrase };
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<AudioTask> { IsSuccess = false, ErrorMessage = ex.Message };
+            }
+        }
+
+
+        public async Task<ApiResponse<Boolean>> DeleteAudioTaskByID(string id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"{_url}/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return new ApiResponse<Boolean> { IsSuccess = true, StatusCode = (int)response.StatusCode };
+                }
+                else
+                {
+                    return new ApiResponse<Boolean> { IsSuccess = false, StatusCode = (int)response.StatusCode, ErrorMessage = response.ReasonPhrase };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<Boolean> { IsSuccess = false, ErrorMessage = ex.Message };
+            }
+        }
+
+
+        public async Task<ApiResponse<AudioTask>> UploadAudioTaskAsync(IBrowserFile file)
+        {
+            try
+            {
+                using var content = new MultipartFormDataContent();
+
+                long maxFileSize = 1024 * 1024 * 50;
+
+                var fileStreamContent = new StreamContent(file.OpenReadStream(maxAllowedSize: maxFileSize));
+
+                fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(file.ContentType);
+
+                content.Add(fileStreamContent, "file", file.Name);
+
+                var response = await _httpClient.PostAsync($"{_url}", content);
 
                 if (response.IsSuccessStatusCode)
                 {
